9b8767aac38b1eb1bb1b3b34e2641382d25da300
irisbune <irisbune@gmail.com>
2016-03-04 20:34:48 +0100

    setup routes

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Home extends React.Component {
+  constructor(){
+    super();
+  }
+  render(){
+    return(
+      <div>
+        
+
+      </div>
+    );
+  }
+
+}
+export default Home;
