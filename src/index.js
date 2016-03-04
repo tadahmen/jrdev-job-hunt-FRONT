53bda8247cac399cce3e6c47f13e7cfcca7307f5
tadahmen <tadahmen@gmail.com>
2016-03-04 20:11:33 +0100

    JuniorForm grotendeels af

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
+import JuniorForm from './JuniorForm'    // <---------------- deze veranderen
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<JuniorForm />, document.getElementById('root'));    // <---------------App weer terugzetten waar nu JuniorForm staat
