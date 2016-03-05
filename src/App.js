2603b7378a4c125e9636524688cea7dc71b4a940
irisbune <irisbune@gmail.com>
2016-03-05 09:52:01 +0100

    set up company and company_profile directories with basic components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import jQuery from 'jquery';
 import Navigation from './Navigation';
 
 class App extends React.Component {
+
+  // constructor(){
+  //   super();
+  //
+  //   this.state = {
+  //     signedIn: null
+  //   }
+  // }
+  // componentWillMount(){
+  //   let component = this;
+  //
+  //   jQuery.ajax({
+  //     method: "GET",
+  //     url: "http://localhost:8888/auth/is_signed_in"
+  //   }).done(function(data){
+  //     console.log(data);
+  //     console.log("yes, checked auth");
+  //     component.setState({
+  //       signedIn: data.signed_in
+  //     });
+  //   }).fail(function(data){
+  //     console.log(data);
+  //   })
+  }
+
     render() {
         return (
           <div>
